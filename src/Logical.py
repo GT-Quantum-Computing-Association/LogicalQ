3a3b8d4c74228ceff28feba987f2dd72e6970ee2
David Lloyd George <dlloydgeorge20@gmail.com>
2024-02-15 18:57:19 -0500

    Updates to the Logical

--- src/Logical.py
+++ src/Logical.py
@@ -1,31 +1,36 @@
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
-
+from qiskit.quantum_info import Pauli
 class LogicalQubit:
     # @TODO - Add a unique identifier of some sorts for multi-logical qubit circuits
     qreg = QuantumRegister(7, "logical_quantum")
     creg = ClassicalRegister(3, "logical_classical")
     qcirc = QuantumCircuit(qreg)
 
-    def __init__(self, initial_state=0):
+    def __init__(self, stabilizers: list, initial_state=0):
+        N_stab = len(stabilizers)
+        N_q = len(stabilizers[0])
+        self.stabilizer_list = stabilizers
         self.initial_state = initial_state
-
-        self.encode(initial_state)
+        self.qreg = QuantumRegister(N_q, "logical_quantum")
+        self.creg = ClassicalRegister(3, "logical_classical")
+        self.qcirc = QuantumCircuit(self.qreg)
 
     # @TODO - allow for QECC selection
     def encode(self, initial_state=0, draw=False):
-        qcirc = QuantumCircuit(self.qreg)
-        qcirc.h(self.qreg[0])
-        qcirc.h(self.qreg[1])
-        qcirc.h(self.qreg[3])
-        qcirc.cx(self.qreg[0], self.qreg[2])
-        qcirc.cx(self.qreg[3], self.qreg[5])
-        qcirc.cx(self.qreg[1], self.qreg[6])
-        qcirc.cx(self.qreg[0], self.qreg[4])
-        qcirc.cx(self.qreg[3], self.qreg[6])
-        qcirc.cx(self.qreg[1], self.qreg[5])
-        qcirc.cx(self.qreg[0], self.qreg[6])
-        qcirc.cx(self.qreg[1], self.qreg[2])
-        qcirc.cx(self.qreg[3], self.qreg[4])
+
+        # qcirc = QuantumCircuit(self.qreg)
+        # qcirc.h(self.qreg[0])
+        # qcirc.h(self.qreg[1])
+        # qcirc.h(self.qreg[3])
+        # qcirc.cx(self.qreg[0], self.qreg[2])
+        # qcirc.cx(self.qreg[3], self.qreg[5])
+        # qcirc.cx(self.qreg[1], self.qreg[6])
+        # qcirc.cx(self.qreg[0], self.qreg[4])
+        # qcirc.cx(self.qreg[3], self.qreg[6])
+        # qcirc.cx(self.qreg[1], self.qreg[5])
+        # qcirc.cx(self.qreg[0], self.qreg[6])
+        # qcirc.cx(self.qreg[1], self.qreg[2])
+        # qcirc.cx(self.qreg[3], self.qreg[4])
 
         if initial_state == 1:
             qcirc.x(self.qreg)
