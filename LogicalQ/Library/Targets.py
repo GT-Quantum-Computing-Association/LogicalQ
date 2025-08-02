2b2dc3f89b7f1eb154ce9ef6ed9330f54eb50962
Rasmit Devkota <rdatch101@gmail.com>
2025-08-01 21:59:44 -0400

    generic ion trap Target fix

--- LogicalQ/Library/Targets.py
+++ LogicalQ/Library/Targets.py
@@ -3,7 +3,8 @@ import itertools
 from qiskit.transpiler import Target, InstructionProperties
 from qiskit.circuit import Parameter
 
-from qiskit.circuit.library import RZGate, RZZGate
+from qiskit.circuit.library import Measure, Reset, RZGate, RZZGate
+from qiskit.circuit.controlflow import IfElseOp
 from .Gates import UGate, ZZGate
 
 """
@@ -14,6 +15,27 @@ def construct_target_generic_iontrap(n_qubits=32):
     qubit_list = range(n_qubits)
 
     gates = {
+        "measure": {
+            "n_qubits": 1,
+            "instruction": Measure,
+            "parameters": [],
+            "duration": 25E-6,
+            "error": 1E-4,
+        },
+        "reset": {
+            "n_qubits": 1,
+            "instruction": Reset,
+            "parameters": [],
+            "duration": 25E-6,
+            "error": 1E-4,
+        },
+        "if_else": {
+            "n_qubits": -1,
+            "instruction": IfElseOp,
+            "parameters": ["condition", "true_body", "false_body"],
+            "duration": 0.00,
+            "error": 0.00,
+        },
         "u": {
             "n_qubits": 1,
             "instruction": UGate,
@@ -52,13 +74,17 @@ def construct_target_generic_iontrap(n_qubits=32):
         parameters = []
         for parameter_name in gate_data["parameters"]:
             parameters.append(Parameter(parameter_name))
-        
-        qubit_target_list = list(itertools.product(*([qubit_list]*gate_data["n_qubits"])))
-        
-        gate_properties = {}
-        for qubit_targets in qubit_target_list:
-            gate_properties[qubit_targets] = InstructionProperties(duration=gate_data["error"], error=gate_data["error"])
-        
-        target.add_instruction(gate_data["instruction"](*parameters), gate_properties)
+
+        if gate_data["n_qubits"] == -1:
+            target.add_instruction(gate_data["instruction"]())
+        else:
+            qubit_target_list = list(itertools.product(*([qubit_list]*gate_data["n_qubits"])))
+
+            gate_properties = {}
+            for qubit_targets in qubit_target_list:
+                gate_properties[qubit_targets] = InstructionProperties(duration=gate_data["error"], error=gate_data["error"])
+
+            target.add_instruction(gate_data["instruction"](*parameters), gate_properties)
 
     return target
+
