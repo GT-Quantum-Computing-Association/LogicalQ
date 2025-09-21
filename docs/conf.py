c7397f303fd3e6c2d3371046927852a4ccc6a85f
RasmitDevkota <rdatch101@gmail.com>
2025-09-21 01:27:39 -0400

    added sphinx conf.py for docs

---
+++ docs/conf.py
@@ -0,0 +1,19 @@
+# -- Project information -----------------------------------------------------
+project = "LogicalQ"
+copyright = "2025, GT Quantum Computing Association"
+author = "GT Quantum Computing Association"
+version = release = "7.1.3"
+
+# -- General configuration ------------------------------------------------
+exclude_patterns = ["_build"]
+extensions = []
+language = "en"
+master_doc = "index"
+pygments_style = "sphinx"
+source_suffix = ".py"
+templates_path = ["_templates"]
+
+# -- Options for HTML output ----------------------------------------------
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
